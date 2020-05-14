4965200a80d750ba7cf01212c262c3e1e1eba8c0
Marcel Fabian Krüger <zauguin@gmail.com>
2020-05-14 22:13:40 +0200

    Add upload information

--- build.lua
+++ build.lua
@@ -1,9 +1,24 @@
 module = "lua-uni-algos"
-tdsroot = "luatex"
-installfiles = {"lua-uni-*.lua"}
-sourcefiles = {"lua-uni-*.lua"}
-typesetfiles = {"lua-uni-algos.tex"}
+
 typesetexe = "lualatex"
 stdengine = "luatex"
 checkengines = {"luatex"}
-checkopts = "-shell-escape -interaction=nonstopmode"
+
+installfiles = {"lua-uni-*.lua"}
+sourcefiles = {"lua-uni-*.lua"}
+typesetfiles = {"lua-uni-algos.tex"}
+
+tdsroot = "luatex"
+
+uploadconfig = {
+  pkg = module,
+  version = "v0.1",
+  author = "Marcel Krüger",
+  license = "lppl1.3c",
+  summary = "Unicode algorithms for LuaTeX",
+  ctanPath = "/macros/luatex/generic/lua-uni-algos",
+  update = false,
+  repository = "https://github.com/zauguin/lua-uni-algos",
+  bugtracker = "https://github.com/zauguin/lua-uni-algos/issues",
+  topic = {"luatex", "unicode"},
+}
