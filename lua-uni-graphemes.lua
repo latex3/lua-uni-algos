5e63113010eb9274bd5527c05b4912769e5c3746
Marcel Fabian Krüger <zauguin@gmail.com>
2020-05-14 05:03:15 +0200

    Add license notes

--- lua-uni-graphemes.lua
+++ lua-uni-graphemes.lua
@@ -1,3 +1,18 @@
+-- lua-uni-graphemes.lua
+-- Copyright 2020 Marcel Krüger
+--
+-- This work may be distributed and/or modified under the
+-- conditions of the LaTeX Project Public License, either version 1.3
+-- of this license or (at your option) any later version.
+-- The latest version of this license is in
+--   http://www.latex-project.org/lppl.txt
+-- and version 1.3 or later is part of all distributions of LaTeX
+-- version 2005/12/01 or later.
+--
+-- This work has the LPPL maintenance status `maintained'.
+-- 
+-- The Current Maintainer of this work is Marcel Krüger
+
 local property do
   local p = require'lua-uni-parse'
   local l = lpeg or require'lpeg'
